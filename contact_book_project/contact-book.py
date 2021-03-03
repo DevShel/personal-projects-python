585f297fce18d40be5d22831dd3ddae7360c54cc
Sheldon <55363444+DevShel@users.noreply.github.com>
2021-03-03 00:12:49 -0500

    programmed base of program

---
+++ contact_book_project/contact-book.py
@@ -0,0 +1,26 @@
+import random
+
+contact_list = []
+grab_num = 0
+
+ 
+def contact_grab_num():
+    grab_num = input("What number contact would you like to grab?")
+    return(grab_num)
+def name_add(name_selection):
+    contact_list.insert((len(contact_list)+1),str(name_selection))
+
+
+if ((len(contact_list))>0):
+    print("There are " + str(len(contact_list)) + " contacts to pick from")
+    if (input("Would you like to add a contact? ") =="yes"):
+        name_search = str(input("Type the name of a contact")) 
+    else:
+        grab_num = contact_grab_num()
+else:
+    print("There are no contacts, why don't you add one?")
+    if (input("Would you like to add a contact? ") == "yes"):
+        name_selection = str(input("Type the name of a contact")) 
+        name_add(name_selection)
+
+print(contact_list)
